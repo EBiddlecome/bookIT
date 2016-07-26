9c3fd50616d8dfc4dfe762621b06def8dddac3f9
EBiddlecome <elizabethwbiddlecome@gmail.com>
2016-07-26 14:04:15 -0700

    simple python server

--- drocloud/server.py
+++ drocloud/server.py
@@ -9,43 +9,64 @@ USER_EMAIL      = "elizabethwbiddlecome@gmail.com"
 USER_API_KEY    = "3fe1260c-026e-47df-a038-9a47e46b4c13"
 DRONE_NAME      = 'bobby'
 
-class CustomHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+def getLiveData():
+	url = 'http://cloud.dronesmith.io/api/drone/' + DRONE_NAME + '/live'
 
-    def getLiveData(self):
-        url = 'http://cloud.dronesmith.io/api/drone/' + DRONE_NAME + '/live'
+	headers = {
+		'user-email': 	USER_EMAIL,
+		'user-key': 	USER_API_KEY,
+		'Content-Type':	'application/json'
+	}
 
-        headers = {
-            'user-email':       USER_EMAIL,
-            'user-key':         USER_API_KEY,
-            'Content-Type':     'application/json'
-        }
+	response = requests.get(url, headers=headers)
+	print 'Status code:', response
 
-        response = requests.get(url, headers=headers)
-        jsonText = json.loads(response.text)
-        return jsonText['telemetry']
+	jsonText = json.loads(response.text)
+	print 'Text:', jsonText
 
-    def do_GET(self):
+	return jsonText['telemetry']
+
+print getLiveData
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+httpd = SocketServer.TCPServer(("localhost", PORT), Handler)
+
+# class CustomHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+#     def getLiveData(self):
+#         url = 'http://cloud.dronesmith.io/api/drone/' + DRONE_NAME + '/live'
+
+#         headers = {
+#             'user-email':       USER_EMAIL,
+#             'user-key':         USER_API_KEY,
+#             'Content-Type':     'application/json'
+#         }
+
+#         response = requests.get(url, headers=headers)
+#         jsonText = json.loads(response.text)
+#         return jsonText['telemetry']
+
+#     def do_GET(self):
         
-        if self.path == '/gps':
-            self.send_response(200)
-            self.send_header('Content-type','text/json')
-            self.send_header("Access-Control-Allow-Origin", "*")
-            self.end_headers()
+#         if self.path == '/gps':
+#             self.send_response(200)
+#             self.send_header('Content-type','text/json')
+#             self.send_header("Access-Control-Allow-Origin", "*")
+#             self.end_headers()
 
-            data = self.getLiveData()
+#             data = self.getLiveData()
 
-            r = {
-             'lat': data['GLOBAL_POSITION_INT']['lat'],
-             'lon': data['GLOBAL_POSITION_INT']['lon'],
-             'intensity': data['PAYLOAD']['rads']
-            }
+#             r = {
+#              'lat': data['GLOBAL_POSITION_INT']['lat'],
+#              'lon': data['GLOBAL_POSITION_INT']['lon'],
+#              'intensity': data['PAYLOAD']['rads']
+#             }
 
-            self.wfile.write(json.dumps(r))
-            return
-        else:
-        	SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+#             self.wfile.write(json.dumps(r))
+#             return
+#         else:
+#         	SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
 
-httpd = SocketServer.ThreadingTCPServer(('localhost', PORT), CustomHandler)
+# httpd = SocketServer.ThreadingTCPServer(('localhost', PORT), CustomHandler)
 
 print "Listening on", PORT
 httpd.serve_forever()
